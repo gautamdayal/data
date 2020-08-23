eadb838cd10b36b628b2bfbbbb9922f176e5dd7d
Gautam Dayal <gautam.dayal@yahoo.com>
2020-08-23 16:10:31 +0530

    extracted data and sentiment information

---
+++ trump-rap/analysis.py
@@ -0,0 +1,26 @@
+import pandas as pd
+import numpy as np
+from matplotlib import pyplot
+
+with open('../data/hip-hop-candidate-lyrics/genius_hip_hop_lyrics.csv', 'r', errors='ignore') as f:
+    data = pd.read_csv(f)
+
+# Each element is a list in the form [candidate, sentiment, year]
+sentiments = []
+for i in range(len(data)):
+    sentiments.append([data['candidate'][i], data['sentiment'][i], data['album_release_date'][i]])
+
+trump_sentiments = [l for l in sentiments if l[0]=='Donald Trump']
+counts = {}
+for l in trump_sentiments:
+    year = l[2]
+    if year not in counts:
+        counts[year]=[0, 0, 0]
+    if l[1] == 'positive':
+        counts[year][0] += 1
+    elif l[1] == 'neutral':
+        counts[year][1] += 1
+    elif l[1] == 'negative':
+        counts[year][2] += 1
+
+print(counts)
