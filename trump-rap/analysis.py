cd567b965adfe79c1efc6082f2ad2797b2c749c8
Gautam Dayal <gautam.dayal@yahoo.com>
2020-08-23 20:35:06 +0530

    added sentiment line plot

--- trump-rap/analysis.py
+++ trump-rap/analysis.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
-from matplotlib import pyplot
+import matplotlib.pyplot as plt
+import collections
 
 with open('../data/hip-hop-candidate-lyrics/genius_hip_hop_lyrics.csv', 'r', errors='ignore') as f:
     data = pd.read_csv(f)
@@ -23,4 +24,12 @@ for l in trump_sentiments:
     elif l[1] == 'negative':
         counts[year][2] += 1
 
-print(counts)
+o_counts = collections.OrderedDict(sorted(counts.items()))
+
+plt.style.use('fivethirtyeight')
+plt.plot(list(o_counts.keys()), [l[0] for l in list(o_counts.values())], label='positive')
+plt.plot(list(o_counts.keys()), [l[1] for l in list(o_counts.values())], label='neutral')
+plt.plot(list(o_counts.keys()), [l[2] for l in list(o_counts.values())], label='negative')
+plt.legend()
+plt.title('Mentions of Donald Trump in Hip-Hop')
+plt.show()
