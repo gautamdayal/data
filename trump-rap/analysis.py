313d9731f78b4db781a0b66a71b9ea1f57ad777a
Gautam Dayal <gautam.dayal@yahoo.com>
2020-08-25 21:31:50 +0530

    added barplot

--- trump-rap/analysis.py
+++ trump-rap/analysis.py
@@ -27,9 +27,30 @@ for l in trump_sentiments:
 o_counts = collections.OrderedDict(sorted(counts.items()))
 
 plt.style.use('fivethirtyeight')
-plt.plot(list(o_counts.keys()), [l[0] for l in list(o_counts.values())], label='positive')
-plt.plot(list(o_counts.keys()), [l[1] for l in list(o_counts.values())], label='neutral')
-plt.plot(list(o_counts.keys()), [l[2] for l in list(o_counts.values())], label='negative')
+
+barWidth = 0.25
+
+# set height of bar
+bars1 = [l[0] for l in list(o_counts.values())]
+bars2 = [l[1] for l in list(o_counts.values())]
+bars3 = [l[2] for l in list(o_counts.values())]
+
+# Set position of bar on X axis
+r1 = np.arange(len(bars1))
+r2 = [x + barWidth for x in r1]
+r3 = [x + barWidth for x in r2]
+
+# Make the plot
+plt.bar(r1, bars1, color='green', width=barWidth, edgecolor='white', label='Positive')
+plt.bar(r2, bars2, color='grey', width=barWidth, edgecolor='white', label='Neutral')
+plt.bar(r3, bars3, color='red', width=barWidth, edgecolor='white', label='Negative')
+
+# Add xticks on the middle of the group bars
+plt.xlabel('Year')
+plt.ylabel('Number of Songs')
+plt.xticks([r + barWidth for r in range(len(bars1))], list(o_counts.keys()), rotation=45)
+
+plt.title('Mentions of Donald Trump in Rap Songs by Sentiment')
 plt.legend()
-plt.title('Mentions of Donald Trump in Hip-Hop')
+plt.tight_layout()
 plt.show()
